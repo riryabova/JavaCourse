1b9552e22d600d7edff1af38b656cc44d2691416
Ri <riryabova@mail.ru>
2019-10-26 10:42:16 +0300

    Add Lesson13

---
+++ src/Lesson13.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Lesson13 {
+    private String s;
+
+    //Необходимо провести сравнение длины двух строк, которые были введены с клавиатуры и записаны
+    // в соответствующие переменные. Вывести на экран строку с наибольшей длиной. Если длины равны, вывести соответствтующее сообщение.
+    public static void main(String[] args) {
+        Lesson13 s1 = new Lesson13();
+        s1.input(s1);
+        Lesson13 s2 = new Lesson13();
+        s2.input(s2);
+
+        int len1 = s1.s.length();
+        int len2 = s2.s.length();
+
+        if (s1.s.length() == s2.s.length()) {
+            System.out.println("Lengths equal");
+        } else if (len1 > len2) {
+            System.out.println("First string = " + s1.s);
+        } else {
+            System.out.println("Second string = " + s2.s);
+        }
+    }
+
+    void input(Lesson13 l13) {
+        String s;
+        Scanner inputS = new Scanner(System.in);
+        System.out.println("Input your string");
+        s = inputS.nextLine();
+        this.s = s;
+        return;
+    }
+}
+
+
+
